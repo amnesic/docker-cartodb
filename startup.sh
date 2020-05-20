2531862f545029d285ad24afccce29f3a8846674
kidwellj <j.kidwell@bham.ac.uk>
2020-05-20 09:07:57 +0100

    updated startup.sh for production

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 export CARTO_HOSTNAME=${CARTO_HOSTNAME:=$HOSTNAME}
 
-perl -pi -e 's/cartodb\.localhost/$ENV{"CARTO_HOSTNAME"}/g' /etc/nginx/nginx.conf /cartodb/config/app_config.yml /Windshaft-cartodb/config/environments/development.js
+perl -pi -e 's/cartodb\.localhost/$ENV{"CARTO_HOSTNAME"}/g' /etc/nginx/nginx.conf /cartodb/config/app_config.yml /Windshaft-cartodb/config/environments/production.js
 
 PGDATA=/var/lib/postgresql
 if [ "$(stat -c %U $PGDATA)" != "postgres" ]; then
@@ -17,10 +17,10 @@ service redis-server start
 service nginx start
 
 cd /Windshaft-cartodb
-node app.js development &
+node app.js production &
 
 cd /CartoDB-SQL-API
-node app.js development &
+node app.js production &
 
 cd /cartodb
 bundle exec script/restore_redis
@@ -28,7 +28,7 @@ bundle exec script/resque > resque.log 2>&1 &
 script/sync_tables_trigger.sh &
 
 # Recreate api keys in db and redis, so sql api is authenticated
-echo 'delete from api_keys' | psql -U postgres -t carto_db_development
+echo 'delete from api_keys' | psql -U postgres -t carto_db_production
 bundle exec rake carto:api_key:create_default
 
 # bundle exec rake carto:api_key:create_default
