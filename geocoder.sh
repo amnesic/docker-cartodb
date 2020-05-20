fe203581e784d9bb26e97b3b8bb74a92c1240948
kidwellj <j.kidwell@bham.ac.uk>
2020-05-20 09:01:58 +0100

    changed geocoder settings to work in production

--- geocoder.sh
+++ geocoder.sh
@@ -12,28 +12,28 @@ bundle exec  rake cartodb:db:set_user_quota[geocoder,102400]
 echo "--- Allowing unlimited tables creation"
 bundle exec  rake cartodb:db:set_unlimited_table_quota[geocoder]
 
-GEOCODER_DB=`echo "SELECT database_name FROM users WHERE username='geocoder'" | psql -U postgres -t carto_db_development`
+GEOCODER_DB=`echo "SELECT database_name FROM users WHERE username='geocoder'" | psql -U postgres -t carto_db_production`
 psql -U postgres $GEOCODER_DB < /cartodb/script/geocoder_server.sql
 
 # Import observatory test dataset
 psql -U postgres -d $GEOCODER_DB -f /observatory-extension/src/pg/test/fixtures/load_fixtures.sql
 # Setup permissions for observatory
-psql -U postgres -d $GEOCODER_DB -c "BEGIN;CREATE EXTENSION IF NOT EXISTS observatory VERSION 'dev'; COMMIT" -e
+psql -U postgres -d $GEOCODER_DB -c "BEGIN;CREATE EXTENSION IF NOT EXISTS observatory VERSION 'prod'; COMMIT" -e
 psql -U postgres -d $GEOCODER_DB -c "BEGIN;GRANT SELECT ON ALL TABLES IN SCHEMA cdb_observatory TO geocoder; COMMIT" -e
 psql -U postgres -d $GEOCODER_DB -c "BEGIN;GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA cdb_observatory TO geocoder; COMMIT" -e
 psql -U postgres -d $GEOCODER_DB -c "BEGIN;GRANT SELECT ON ALL TABLES IN SCHEMA observatory TO geocoder; COMMIT" -e
 psql -U postgres -d $GEOCODER_DB -c "BEGIN;GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA observatory TO geocoder; COMMIT" -e
 
 # Setup dataservices client
-# dev user
-USER_DB=`echo "SELECT database_name FROM users WHERE username='dev'" | psql -U postgres -t carto_db_development`
+# prod user
+USER_DB=`echo "SELECT database_name FROM users WHERE username='prod'" | psql -U postgres -t carto_db_production`
 echo "CREATE EXTENSION cdb_dataservices_client;" | psql -U postgres $USER_DB
-echo "SELECT CDB_Conf_SetConf('user_config', '{"'"is_organization"'": false, "'"entity_name"'": "'"dev"'"}');" | psql -U postgres $USER_DB
+echo "SELECT CDB_Conf_SetConf('user_config', '{"'"is_organization"'": false, "'"entity_name"'": "'"prod"'"}');" | psql -U postgres $USER_DB
 echo -e "SELECT CDB_Conf_SetConf('geocoder_server_config', '{ \"connection_str\": \"host=localhost port=5432 dbname=${GEOCODER_DB# } user=postgres\"}');" | psql -U postgres $USER_DB
-bundle exec  rake cartodb:services:set_user_quota['dev',geocoding,100000]
+bundle exec  rake cartodb:services:set_user_quota['prod',geocoding,100000]
 
 # example organization
-ORGANIZATION_DB=`echo "SELECT database_name FROM users WHERE username='admin4example'" | psql -A -U postgres -t carto_db_development`
+ORGANIZATION_DB=`echo "SELECT database_name FROM users WHERE username='admin4example'" | psql -A -U postgres -t carto_db_production`
 echo "CREATE EXTENSION cdb_dataservices_client;" | psql -U postgres $ORGANIZATION_DB
 echo "SELECT CDB_Conf_SetConf('user_config', '{"'"is_organization"'": true, "'"entity_name"'": "'"example"'"}');" | psql -U postgres $ORGANIZATION_DB
 echo -e "SELECT CDB_Conf_SetConf('geocoder_server_config', '{ \"connection_str\": \"host=localhost port=5432 dbname=${GEOCODER_DB# } user=postgres\"}');" | psql -U postgres $ORGANIZATION_DB
